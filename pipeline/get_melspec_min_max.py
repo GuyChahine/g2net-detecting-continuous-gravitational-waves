66c9881f7dbaade21843ababe6291d5cba587823
GuyChahine <guychahine@gmail.com>
2022-10-11 18:42:32 +0200

    fix: imports

--- pipeline/get_melspec_min_max.py
+++ pipeline/get_melspec_min_max.py
@@ -4,12 +4,12 @@ import json
 import sys
 sys.path.insert(0, "\\".join(__file__.split("\\")[:__file__.split("\\").index("g2net-detecting-continuous-gravitational-waves")+1]))
 
-from src.data_preparation import DataTransformation
+from src.mel_spectrogram import MelSpectrogram_v1
 from src.classic_data import batch_generator
 from src.utils import get_file_names
 
 def get_min_man_melpec(sfts):
-    dt = DataTransformation()
+    dt = MelSpectrogram_v1()
     magnitude, phase = dt._magphase(sfts)
     mel_spec = dt._melspectrogram(magnitude)
     return mel_spec.min(), mel_spec.max(), mel_spec.shape[1]
