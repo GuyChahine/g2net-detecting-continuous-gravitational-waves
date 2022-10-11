b11e97ddbee9e9d876aba61a3a5f44aa716f7cb6
GuyChahine <guychahine@gmail.com>
2022-10-11 18:28:09 +0200

    refactor: class to create custom direclty into data preparator

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,17 +1,25 @@
 import librosa
+import torch
 import numpy as np
 import pandas as pd
+
+from src.utils import get_file_names, read_hdf5, find_target
+
+def dataset_split(dataset: torch.utils.data.Dataset, valid_size: float):
+    train_length = int(len(dataset) * (1 - valid_size))
+    valid_length = int(len(dataset) * valid_size)
+    return torch.utils.data.random_split(dataset, [train_length, valid_length])
 
 class DataCleaning():
     
     def _min_max_scaler(self, arr: np.ndarray, min: float, max: float):
         return (arr - min) / (max-min)
 
-    def _resizer(self, arr: np.ndarray, min: float):
-        return np.delete(arr, slice(min, arr.shape[1]), axis=1)
+    def _resizer(self, arr: np.ndarray, min_shape: int):
+        return np.delete(arr, slice(min_shape, arr.shape[1]), axis=1)
     
-    def _reshaper(self, arr: np.ndarray):
-        return arr.reshape((1, 128, -1))
+    def _reshaper(self, arr: np.ndarray, new_shape: tuple):
+        return arr.reshape(new_shape)
 
 class DataTransformation():
         
@@ -23,8 +31,33 @@ class DataTransformation():
     
 class MelSpectrogram_v1(DataTransformation, DataCleaning):
     
-    def __init__(self, min_max_melspectrogram: pd.DataFrame):
-        self.min_max_melspectrogram = min_max_melspectrogram
+    def __init__(
+        self,
+        root: str = "data/raw/train/",
+        labels_path: str = "data/raw/train_label/_labels.csv",
+        min_max_sg_path: str = "data/infos/min_max_melspectrogram.csv",
+    ):
+        self.file_names = get_file_names(root)
+        self.labels = pd.read_csv(labels_path)
+        
+        self.min_max_melspectrogram = pd.read_csv(min_max_sg_path)
+    
+    def __len__(self):
+        return self.file_names.size
+    
+    def __getitem__(self, idx: int):
+        (name,
+        h1_sfts,
+        _h1_timestamp,
+        l1_sfts,
+        _l1_timestamp,
+        _frenquency) = read_hdf5(self.file_names[idx]).values()
+        
+        target = find_target(name, self.labels)
+        
+        sg_h1, sg_l1 = self.fit(h1_sfts, l1_sfts)
+        
+        return torch.Tensor(sg_h1), torch.Tensor(sg_l1), torch.Tensor([target])
     
     def _get_melspectrogram(self, sfts):
         magnitude, _phase = self._magphase(sfts)
@@ -48,8 +81,8 @@ class MelSpectrogram_v1(DataTransformation, DataCleaning):
             float(self.min_max_melspectrogram.l1_max_melspec)
         )
         
-        sg_h1 = self._reshaper(sg_h1)
-        sg_l1 = self._reshaper(sg_l1)
+        sg_h1 = self._reshaper(sg_h1, (1, *sg_h1.shape))
+        sg_l1 = self._reshaper(sg_l1, (1, *sg_l1.shape))
         
         return sg_h1, sg_l1
         
