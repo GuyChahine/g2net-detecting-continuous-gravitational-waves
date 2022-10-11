b08ad6af530e3af9bd7e0c487b86a6c188f089ff
GuyChahine <guychahine@gmail.com>
2022-10-11 18:30:41 +0200

    fix: import due to refactor of dataset_plit function

--- pipeline/run_model.py
+++ pipeline/run_model.py
@@ -9,7 +9,8 @@ import wandb
 import sys
 sys.path.insert(0, "\\".join(__file__.split("\\")[:__file__.split("\\").index("g2net-detecting-continuous-gravitational-waves")+1]))
 
-from src.data_preparation import MelSpectrogram_v1, dataset_split
+from src.data_preparation import MelSpectrogram_v1
+from src.utils import dataset_split
 from model.model_architecture import NeuralNet_v1
 from model.step_epoch import train
 
