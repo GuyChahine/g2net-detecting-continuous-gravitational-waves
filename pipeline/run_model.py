e283a09c22b1857205c0159c46b451f3d3873c84
GuyChahine <guychahine@gmail.com>
2022-10-20 18:35:44 +0200

    edit: file to run with new magnitude model

--- pipeline/run_model.py
+++ pipeline/run_model.py
@@ -9,13 +9,13 @@ import wandb
 import sys
 sys.path.insert(0, "\\".join(__file__.split("\\")[:__file__.split("\\").index("g2net-detecting-continuous-gravitational-waves")+1]))
 
-from src.short_time_fourier_transform import ShortTimeFourierTransform_v3
+from src.magnitudes import Magnitude_v1
 from src.utils import dataset_split
-from model.model_short_time_fourier_transform import NeuralNet_v3
+from model.model_magnitudes import NeuralNet_v1
 from model.step_epoch import train
 
 def main():
-    dataset = ShortTimeFourierTransform_v3()
+    dataset = Magnitude_v1()
     train_set, valid_set = dataset_split(dataset, valid_size=CONFIG["valid_size"])
     train_loader = torch.utils.data.DataLoader(
         train_set,
@@ -28,13 +28,13 @@ def main():
         shuffle=True,
     ) if len(valid_set.indices) else None
     
-    model = NeuralNet_v3().to(DEVICE)
+    model = NeuralNet_v1().to(DEVICE)
     optimizer = eval(CONFIG["optimizer"])
     scheduler = CONFIG["scheduler"]
     loss_function = CONFIG["loss_function"]
 
     if REPORTS:
-            wandb.init(project="Test-MNIST", entity="guychahine", config=CONFIG, tags=TAGS)
+        wandb.init(project="Test-MNIST", entity="guychahine", config=CONFIG, tags=TAGS)
             
     for epoch in range(1, CONFIG['nb_epoch']+1):
         train(
@@ -58,8 +58,8 @@ if __name__ == "__main__":
     CONFIG = {
         "nb_epoch": 50,
         "valid_size": 0,
-        "batch_size": 48,
-        "optimizer": "SGD(model.parameters(), lr=1e-3)",
+        "batch_size": 128,
+        "optimizer": "Adam(model.parameters())",
         "scheduler": None,
         "loss_function": binary_cross_entropy,
     }
